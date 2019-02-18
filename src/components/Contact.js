f5295715f8f8e76282598c8f963271cc7666001e
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2019-02-18 14:35:18 -0500

    increase contact page font-size

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,10 +11,10 @@ class Contact extends Component {
                     <br /><br />
                     <center><h3>Thank you for your interest.</h3></center>
                     <br /><br />
-                    <p>Below you fill find my contact information.</p>
-                    <p>My preferred communication method is email. Due to the prevalence of spam-calls nowadays, I don't tend to answer phone calls from unknown numbers.</p>
-                    <p>E-mail: <a href="mailto:brunogarciagonzalez@outlook.com">BrunoGarciaGonzalez@outlook.com</a></p>
-                    <p>Phone: <a href="tel:12402818121">+1 (240) 281 - 8121</a></p>
+                    <p className="contact-body">Below you fill find my contact information.</p>
+                    <p className="contact-body">My preferred communication method is email. Due to the prevalence of spam-calls nowadays, I don't tend to answer phone calls from unknown numbers.</p>
+                    <p className="contact-body">E-mail: <a href="mailto:brunogarciagonzalez@outlook.com">BrunoGarciaGonzalez@outlook.com</a></p>
+                    <p className="contact-body">Phone: <a href="tel:12402818121">+1 (240) 281 - 8121</a></p>
                 </div>
                 <div className="col s3 m3" />
             </div>
