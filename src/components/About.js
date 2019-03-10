46de66b88da767e94bb6d772daa1e4ec7e0b2a4f
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2019-03-10 12:32:17 -0400

    added emoji <span />

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ class About extends Component {
                 <div className="col s3 m3" />
                 <div className="col s6 m6" >
                     <br/><br/>
-                    <center><h3>🇺🇸 🇺🇾</h3></center>
+                    <center><h3><span role="img" aria-label="USA Flag">🇺🇸</span> <span role="img" aria-label="Uruguay Flag">🇺🇾</span></h3></center>
                     <p>Testing...</p>
                 </div>
                 <div className="col s3 m3" />
