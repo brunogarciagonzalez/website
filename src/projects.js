99217aec296c80193222e31fef6b232cd31a2659
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2019-03-10 16:45:56 -0400

    begin projects array

---
+++ src/projects.js
@@ -0,0 +1,108 @@
+import React from "react";
+
+export default [
+    {
+        title: "LinkShare",
+        demo: "https://www.youtube.com/embed/9OvU9JicNrw",
+        description: <div><p>
+            This 3-week project is powered by a Ruby on Rails (API), ReactJS,
+            and Semantic UI.
+            </p>
+            <p>The concept-board for this project includes three things:</p>
+            <ol>
+                <li>The now-gone Yahoo! Directory,</li>
+                <li>The frontpage of the internet: Reddit, and</li>
+                <li>
+                    Amazon and Steam product reviews and review management logic
+              </li>
+            </ol>
+            <p>
+                These three concepts are fused to produce a directory of the
+                internet, with admin-curated sub-directories (i.e. ‘tags’) that
+                are filled with links via user-generated content. These links are
+                each shared through the construction of reviews that have
+                qualitative and quantitative aspects (written content and
+                numerical rating, respectively).
+            </p>
+            <p>
+                In the end, it is an attempt at ‘community-based internet
+                exploration’, a term I came up with to differentiate this idea
+                from crawler-based search engines as a means to navigate the
+                internet. It is very similar to the original attempts at indexing
+                the world wide web, with many additional features centered around
+                user-generated content and extending the user experience beyond
+                anchor tags.
+            </p>
+            <p style={{textAlign: "center"}}>
+                [ <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/brunogarciagonzalez/linkshare-backend"
+                >
+                    Project Back End Code
+              </a>
+                {" // "}
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/brunogarciagonzalez/linkshare-frontend"
+                >
+                    Project Front End Code
+              </a> ]
+            </p></div>
+        
+    },
+    {
+        title: "WeatherScript",
+        demo: "https://www.youtube.com/embed/9OvU9JicNrw",
+        description: <div><p>
+            This 1-week project is powered by a Ruby on Rails (API), ReactJS, Semantic UI, and the MetaWeather API. This was a partner-project with <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/ryan-youngdale-508562112/">Ryan Youngdale</a> as we were learning ReactJS.
+            </p>
+            <p>The concept-board for this project includes one thing:</p>
+            <ol>
+                <li>Apple iPhone's built-in weather application</li>
+            </ol>
+            <p>
+                A weather application that utilizes the <a target="_blank"
+                    rel="noopener noreferrer" href="https://www.metaweather.com/api/">MetaWeather API</a> to showcase 5-day forecasts. API calls to MetaWeather are done through a Rails API application, and memoization is employed therein to minimize calls to MetaWeather. A simple (non-auth) login system is employed to allow users to manage favorite cities, by adding and removing. Finally, the Zomato API was utilized to give users some extra information (best restaurants in the city whose weather is being looked at).
+            </p>
+            <p style={{ textAlign: "center" }}>
+               [ <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/brunogarciagonzalez/weatherScript"
+                >
+                    Project Code
+              </a> ]
+            </p></div>
+
+    },
+    {
+        title: "CryptoHome",
+        demo: "https://www.youtube.com/embed/9OvU9JicNrw",
+        description: <div><p>
+            This 1-week project is powered by a Ruby on Rails (API), vanilla JS, and API. This was a partner-project with <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jerry-w-lee/">Jerry Lee</a> as we were learning Javascript fundamentals.
+            </p>
+            <p>
+                A weather application that utilizes the <a target="_blank" rel="noopener noreferrer" href="https://www.metaweather.com/api/">... API</a> to showcase 5-day forecasts. API calls to MetaWeather are done through a Rails API application, and memoization is employed therein to minimize calls to MetaWeather. A simple (non-auth) login system is employed to allow users to manage favorite cities, by adding and removing. Finally, the Zomato API was utilized to give users some extra information (best restaurants in the city whose weather is being looked at).
+            </p>
+            <p style={{ textAlign: "center" }}>
+                [ <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/brunogarciagonzalez/crypto-home"
+                >
+                    Project Back End Code
+              </a>
+                {" // "}
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/brunogarciagonzalez/crypto-home-frontend"
+                >
+                    Project Front End Code
+              </a> ]
+            </p></div>
+
+    }
+];
