f46f6148f9599d9bb961f0ef8af94fc436aeb61b
Bruno Garcia Gonzalez <brunogarciagonzalez@outlook.com>
2019-05-09 16:00:26 -0400

    responsive: small

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,45 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
+import { Dropdown } from "semantic-ui-react";
 
-const originalState = {
-  toRoot: false,
-  toPortfolio: false,
-  toContact: false,
-  toAbout: false
+const originalState = () => {
+  return(
+    {
+      size: window.innerWidth > 889 ? "regular" : "small",
+      toRoot: false,
+      toPortfolio: false,
+      toContact: false,
+      toAbout: false
+    }
+  )
 };
 
 class Navbar extends Component {
-  state = originalState;
+  state = originalState();
 
   handleLinkClick = e => {
     e.preventDefault();
     let keyString = `to${e.currentTarget.id}`;
     let stateCopy = { ...this.state };
     stateCopy[keyString] = true;
-    this.setState(stateCopy, () => this.setState(originalState));
+    this.setState(stateCopy, () => this.setState(originalState()));
   };
 
   handleResumeClick = e => {
     e.preventDefault();
     window.open("/BrunoGarciaGonzalez_resume.pdf", "_blank");
   };
+
+  componentDidMount() {
+    // event listener on window resize
+    window.onresize = () => {
+      console.log("size: ", window.innerWidth)
+      window.innerWidth > 889 ?
+      this.setState({size: "regular"})
+      :
+      this.setState({size: "small"})
+    }
+  }
 
   render() {
     if (this.state.toRoot) {
@@ -35,59 +52,117 @@ class Navbar extends Component {
       return <Redirect to="/about" />;
     }
 
-    return (
-      <nav>
-        <div className="nav-wrapper teal">
-          <span
-            id="Root"
-            onClick={this.handleLinkClick}
-            className="brand-logo center"
-          >
-            Bruno I. Garcia Gonzalez
-          </span>
-          <ul id="nav-mobile" className="left hide-on-med-and-down">
-            <li>
-              <a
-                href="https://www.linkedin.com/in/garcia-gonzalez/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                LinkedIn
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://medium.com/@brunogarciagonzalez"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Blog
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/brunogarciagonzalez"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                GitHub
-              </a>
-            </li>
-          </ul>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li id="Resume" onClick={this.handleResumeClick}>
-              <a href="/resume">Resume</a>
-            </li>
-            <li id="Portfolio" onClick={this.handleLinkClick}>
-              <a href="/portfolio">Portfolio</a>
-            </li>
-            <li id="Contact" onClick={this.handleLinkClick}>
-              <a href="/contact">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
+    if (this.state.size === "regular") {
+      return (
+        <nav>
+          <div className="nav-wrapper teal">
+            <span
+              id="Root"
+              onClick={this.handleLinkClick}
+              className="brand-logo center"
+            >
+              Bruno I. Garcia Gonzalez
+            </span>
+            <ul id="nav-mobile" className="left">
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/garcia-gonzalez/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LinkedIn
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://medium.com/@brunogarciagonzalez"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Blog
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/brunogarciagonzalez"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a>
+              </li>
+            </ul>
+            <ul id="nav-mobile" className="right">
+              <li id="Resume" onClick={this.handleResumeClick}>
+                <a href="/resume">Resume</a>
+              </li>
+              <li id="Portfolio" onClick={this.handleLinkClick}>
+                <a href="/portfolio">Portfolio</a>
+              </li>
+              <li id="Contact" onClick={this.handleLinkClick}>
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      );
+    } else {
+      return (
+        <nav>
+          <div className="nav-wrapper teal">
+            <span
+              id="Root"
+              onClick={this.handleLinkClick}
+              className="brand-logo center"
+            >
+              Bruno I. Garcia Gonzalez
+            </span>
+            <ul id="nav-mobile" className="left">
+              <li>
+                <a className="dropDownItem">
+                  <Dropdown item text='Menu'>
+                   <Dropdown.Menu>
+                     <a id="Resume" href="/resume"onClick={this.handleResumeClick}>
+                     <Dropdown.Item>Resume</Dropdown.Item>
+                     </a>
+                     <a
+                     href="https://www.linkedin.com/in/garcia-gonzalez/"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     >
+                      <Dropdown.Item>LinkedIn</Dropdown.Item>
+                     </a>
+                     <a
+                     href="https://github.com/brunogarciagonzalez"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     >
+                     <Dropdown.Item>GitHub</Dropdown.Item>
+                     </a>
+                     <a
+                       href="https://medium.com/@brunogarciagonzalez"
+                       target="_blank"
+                       rel="noopener noreferrer"
+                     >
+                       <Dropdown.Item>Blog</Dropdown.Item>
+                     </a>
+                     <a id="Portfolio" href="/portfolio" onClick={this.handleLinkClick}>
+                      <Dropdown.Item>Portfolio</Dropdown.Item>
+                     </a>
+                     <a id="Contact" href="/contact" onClick={this.handleLinkClick}>
+                     <Dropdown.Item>Contact</Dropdown.Item>
+                     </a>
+                   </Dropdown.Menu>
+                  </Dropdown>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      );
+    }
+
+
   }
 }
 
