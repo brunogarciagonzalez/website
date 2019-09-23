ef0d89f990c927738148eb4c39442c2ec4e15e3b
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2019-09-22 20:06:38 -0400

    update HeroBlurb

---
+++ src/components/CardText.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default class CardText extends React.Component {
+  render() {
+    return (
+      <span>
+        Hello! <br /> <br />
+        I am a software developer in the DMV, with proficiency in ReactJS &&
+        Ruby on Rails. I am interested in contributing to great products,
+        original projects, and noble causes. <br />
+        <br />I am a lifelong student who excels at building workplace cohesion
+        through my strong communication skills and positive energy, and can fit
+        well within various team dynamics.
+        <br />
+        <br />
+        Regards, and wishing you peace, <br />
+        Bruno
+      </span>
+    );
+  }
+}
--- src/components/HeroBlurb.js
+++ src/components/HeroBlurb.js
@@ -1,49 +1,40 @@
 import React, { Component } from "react";
 import { Image } from "semantic-ui-react";
+import CardText from "./CardText.js";
 
 class HeroBlurb extends Component {
   render() {
-
     if (this.props.size === "x-small") {
       return (
         <div>
           <div className="row">
             <div className="row">
-              <div className="col" style={{width:"100%"}}>
+              <div className="col" style={{ width: "100%" }}>
                 <br />
                 <br />
                 <br />
-                <center><span>
-                  <Image
-                    src="/profile_photo_min.jpg"
-                    size="medium"
-                    circular
-                    style={{ display: "inline-block !important"}}
-                  />
-                </span></center>
+                <center>
+                  <span>
+                    <Image
+                      src="/profile_photo_min.jpg"
+                      size="medium"
+                      circular
+                      style={{ display: "inline-block !important" }}
+                    />
+                  </span>
+                </center>
               </div>
             </div>
             <div className="col">
               <div className="card">
                 <div className="card-content">
                   <p className="blurbStyle">
-                    Hello! <br />
-                    <br />
-                    I am a software developer in the DMV, with proficiency in Ruby
-                    on Rails && ReactJS and a passion for backend development.
-                    I am interested in contributing to great products, original projects, and noble causes.
-
-                    I am a lifelong student who excels at building workplace cohesion through my strong communication skills and positive energy, and can fit well within various team dynamics.
-                     <br />
-                    <br />
-                    Regards, and wishing you peace, <br />
-                    Bruno
+                    <CardText />
                   </p>
                 </div>
               </div>
             </div>
           </div>
-
         </div>
       );
     } else {
@@ -68,17 +59,7 @@ class HeroBlurb extends Component {
             <div className="card">
               <div className="card-content">
                 <p className="blurbStyle">
-                  Hello! <br />
-                  <br />
-                  I am a software developer in the DMV, with proficiency in Ruby
-                  on Rails && ReactJS and a passion for backend development.
-                  I am interested in constributing to great products, original projects, and noble causes.
-
-                  I am a lifelong student who excels at building workplace cohesion through my strong communication skills and positive energy, and can fit well within various team dynamics.
-                   <br />
-                  <br />
-                  Regards, and wishing you peace, <br />
-                  Bruno
+                  <CardText />
                 </p>
               </div>
             </div>
@@ -87,7 +68,6 @@ class HeroBlurb extends Component {
         </div>
       );
     }
-
   }
 }
 
